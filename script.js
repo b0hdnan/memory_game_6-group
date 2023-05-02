e535c6e5eaec98fa0f2cb245ad854a9bd20c010c
KarmanniyMukuta <gen.nefchev@gmail.com>
2023-05-02 22:27:02 +0300

    94%

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
 const board = document.querySelector(".board");
 let persons = [
   {
-    name: "Богдан Хмельницький \n JIIOOOOOOOOOO OIIII HIOHIH HIOHIOH IOIOIHIB IIHIOH GGOIHG",
+    description: "Богдан Хмельницький (за гравюрою В.Гондіуса). З 1648 року — гетьман Війська Запорозького \n Організатор повстання проти панування шляхти в Україні, яке переросло у Національно-визвольну\n війну українського народу проти Речі Посполитої.",
+    name: "Богдан Хмельницький",
     img: "./img/Bogdan_Hmelnitski.jpg"
   },
   {
+    description: "Йосиф Сліпий після смерті А.Шептицького, у листопаді 1944р. став главою греко-католицької\n церкви. У 1946р. звинувачений у \«ворожій діяльності проти УРСР, співпраці з німецькофашистськими окупантами\», засуджений до ув’язнення. Після вісімнадцяти років каторги звільнений\n у 1963р.",
     name: "Йосиф Сліпий",
     img: "./img/yosif.png"
   },
   {
+    description: "Тарас Шевченко – У 1840 р. у Петербурзі вийшла перша збірка його поезій \"Кобзар\". Був одним з\n найвидатніших майстрів українського образотворчого мистецтва. В 1844р. вийшов альбом офортів\n «Живописна Україна», де представлені роботи художника на історичну тематику («Дари в Чигирині»,\n «Смерть Б.Хмельницького»)",
     name: "Тарас Шевченко",
     img: "./img/taras_shevchenko.png"
   },
   {
+    description: "Павло Тетеря — український дипломат, військовий, політичний і державний діяч. Гетьман Війська Запорозького, голова козацької держави Правобережної України (1663–1665).",
     name: "Павло Тетеря",
     img: "./img/Pavlo_Teteri.jpg"
   },
   {
+    description: "Іван Франко – один із засновників Русько-української радикальної партії (РУРП), згодом відмовився\n від соціалістичних поглядів і став членом Української національно-демократичної партії (УНДП).\n Очолював в НТШ філологічну секцію. Літературні творчість розпочав у 70-х роках. У 1892р. брав\n участь у селянському віче в м.Снятин",
     name: "Іван Франко",
     img: "./img/ivan_franko.png"
   },
   {
+    description: "Олесь Гончар – український прозаїк, найвідоміші твори «Прапороносці», «Тронка», «Людина ізброя».\n У 1968р. виходить його роман «Собор», за цей твір письменник був підданий критиці, а твір\n вилучений з літературного процесу на два десятиліття. Після розпаду Радянського Союзу, став\n активістом у створенні Товариства української мови і Народного Руху України.",
     name: "Олесь Гончар",
     img: "./img/oles.png"
   },
   {
+    description: "Василь Стус – один з активних учасників акції протесту у вересні 1965р. в кінотеатрі «Україна» в\n Києві, за що був виключений з аспірантури Інституту літератури ім. Т.Шевченка АН УРСР. У 1970р.\n за кордоном вийшла його збірка поезій \«Зимові дерева\», через два роки був засуджений за\n \«антирадянську агітацію\».",
     name: "Василь Стус",
     img: "./img/vasil.png"
   },
   {
+    description: "В’ячеслав Чорновіл у 1967р. підготував збірку «Лихо з розуму», яку відзначено премією\n міжнародної журналістики. За це був заарештований і засуджений до 3 років суворого режиму. У\n 1970р. розпочинає видання самвидавського журналу \«Український вісник\».",
     name: "В’ячеслав Чорновіл",
     img: "./img/vyacheslav.png"
   },
   {
+    description: "Іван Дзюба – автор книги «Інтернаціоналізм чи русифікація» (1965р.). За відкриті виступи на захист\n української інтелігенції, самвидавські матеріали був ув’язнений у 1972р. Брав участь у створені\n Народного Руху України за перебудову (1989р.). ",
     name: "Іван Дзюба",
     img: "./img/ivan.png"
   },
   {
+    description: "Іван Сірко — козацький ватажок, легендарний кошовий отаман Запорозької Січі й усього Війська Запорозького Низового. Здобув перемогу в 65 боях. Герой багатьох українських пісень і казок. Після своєї смерті вважався характерником.",
     name: "Іван Сірко",
     img: "./img/Ivan_Sirko.png"
   },
   {
+    description: "Петро Григоренко – генерал-майор, активний учасник дисидентського руху. Відстоював права\n кримських татар на повернення на батьківщину. Учасник Української громадської групи сприяння\n виконання Гельсінських угод (УГГ, 1976р.). двічі був арештований, перебував спецпсихіатричних\n лікарнях Радянського Союзу.",
     name: "Петро Григоренко",
     img: "./img/petr.png"
   },
   {
+    description: "Іван Миколайчук – український кіноактор, кінорежисер, сценарист. Провідні ролі у фільмах «Тіні\n забутих предків» (реж. С. Параджанов), «Пропала грамота» (реж. Б. Івченко), «Білий птах з чорною\n ознакою» (реж. Ю. Іллєнко). Як режисер – фільм «Вавилон ХХ» (1979р.)",
     name: "Іван Миколайчук",
     img: "./img/ivanm.png"
   }
@@ -89,6 +101,7 @@ function choose() {
 		console.log(chosen)
     console.log(`url(${persons[this.dataset.value].img})`)
     desc = (persons[this.dataset.value].name)
+    dd = (persons[this.dataset.value].description)
     
 	}
 	if (chosen.length === 2){
@@ -104,6 +117,7 @@ function check(){
     clearInterval(Interval); // 2 card and stop
     console.log(r++)
     console.log(desc)
+    alert(dd)
        
 
     if (r == 13){           // no cards
@@ -159,6 +173,7 @@ let r = 1
   	min = "00";
     appendSec.innerHTML = sec;
   	appendMin.innerHTML = min;
+    persons = shuffle(persons);
   }
   
    
@@ -189,65 +204,4 @@ let r = 1
   }
   
 
-//jjj
-
-/*function soundClick() {*/
-
-  //const audio = new Audio(); // Создаём новый элемент Audio
-  //audio.src = '/mp/apovabin-around-the-phonk.mp3'; // Указываем путь к звуку "клика"
-  //audio.autoplay = true; // Автоматически запускаем
-
-  /*if(f % 2 == 0) {
-    audio.play();
-  } else {
-    audio.pause();
-  }*/
- 
-//} 
- 
-
-
-/*let audio = new Audio();
-audio.volume = .25; // Чтобы не испугать пользователя
-
-document.querySelector('#play').addEventListener('click', function(e){
-  if(e.target.value === 'Воспроизвести') {
-    e.target.value = 'Остановить';
-    audio.src = e.target.getAttribute('data-url');
-    audio.play();
-  } else {
-    e.target.value = 'Воспроизвести';
-    audio.currentTime = 0;
-    audio.pause();
-  }
-});
-*/
-/* playPauseButton = document.querySelector('#playPauseButton')
- rightButton = document.querySelector('#rightButton')
- leftButton = document.querySelector("#leftButton")
-const audio = new Audio()
-let array = ['/mp/apovabin-around-the-phonk.mp3', '/mp/bozhe_yake_konchene.mp3'];
-audio.src = '/mp/apovabin-around-the-phonk.mp3';
-let index = 0;
-rightButton.onclick = function(){selecting_track(1)};
-leftButton.onclick = function(){selecting_track(-1)};
-function selecting_track(direction){
-    index += direction;
-    if(index >= array.length) index = 0;
-    if(index < 0) index = array.length - 1;
-    audio.src = url + array[index];
-    audio.play();
-    playPauseButton.innerText = 'pause';
-     // ... тут меняем надписи на элементах ...
-}
-
-function run(){
-    if(audio.paused){
-        audio.play();
-        this.innerText = 'pause';
-    }else{
-        audio.pause();
-        this.innerText = 'play';
-    }
-}
-*/let audio = new Audio()
+let audio = new Audio()
